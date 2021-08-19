f3f1b720d68e6d42b019fa66ed384409d0803c72
Sauman Das <saumand9424@gmail.com>
2021-08-19 14:08:50 -0400

    added respiratory preprocess

--- utils/Preprocessing.py
+++ utils/Preprocessing.py
@@ -1,1 +1,21 @@
-print('hello world')
+import librosa as lb
+import numpy as np
+
+
+# Helper method for respiratory_preprocess
+def get_feature_helper(path):
+    soundArr,sample_rate=lb.load(path)
+    mfcc=lb.feature.mfcc(y=soundArr,sr=sample_rate)
+    cstft=lb.feature.chroma_stft(y=soundArr,sr=sample_rate)
+    mSpec=lb.feature.melspectrogram(y=soundArr,sr=sample_rate)
+    return mfcc,cstft,mSpec
+
+
+def respiratory_preprocess(vid_file_path):
+    a, b, c = get_feature_helper(vid_file_path)
+    mfcc = np.array([a])
+    cstft = np.array([b])
+    mspec = np.array([c])
+    return {"mfcc":mfcc,"croma":cstft,"mspec":mspec}
+
+
