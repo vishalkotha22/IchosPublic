9d9de39ff8667c9fc5ed8eaccb1baa1a5726e573
Sameer Gabbita <57297991+sameerg47@users.noreply.github.com>
2021-08-28 17:32:10 -0400

    Add files via upload

---
+++ app/pages/app/pages/b.py
@@ -0,0 +1,5 @@
+import streamlit as st
+import numpy as np
+
+def app():
+    st.title('SLI')
---
+++ app/pages/app/pages/h.py
@@ -0,0 +1,7 @@
+import streamlit as st
+
+
+def app():
+    st.title("[Insert App name here]")
+    st.write("This web application can diagnose some of the most progressive diseases.")
+    st.write('Built by: Sauman Das, Sameer Gabbita, Arnav Jain, & Vishal Kotha')
