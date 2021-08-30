e1c2b62ec5336c518a1953f41f5031b21138c72b
Sameer Gabbita <57297991+sameerg47@users.noreply.github.com>
2021-08-29 20:21:21 -0400

    added get_model and file upload stuff

--- app/pages/c.py
+++ app/pages/c.py
@@ -1,12 +1,20 @@
 import streamlit as st
-import tensorflow as tf
 import numpy as np
+import tensorflow as tf
+import librosa
+from pydub import AudioSegment
+import wave
+from scipy.io import wavfile
 
+@st.cache(allow_output_mutation=True)
+def get_model():
+    model = tf.keras.models.load_model('models/respiratory_model.pb')
+    return model
 
 def app():
     st.title('Respiratory Diseases')
-
-
-
-
-
+    file = st.file_uploader("Please upload an audio recording", type=["wav"])
+    if file is None:
+        pass
+    else:
+        # inference stuff here (display the audio file and run the model)
