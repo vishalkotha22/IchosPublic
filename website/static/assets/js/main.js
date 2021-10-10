1f316327691a282b45b6e12ca5382f91ccd46eea
Arnav Jain <arnavj22@gmail.com>
2021-10-09 22:03:59 -0400

    made upload look hot

--- website/static/assets/js/main.js
+++ website/static/assets/js/main.js
@@ -3,7 +3,22 @@
 	html5up.net | @ajlkn
 	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
 */
+var input = document.getElementById( 'files' );
+var infoArea = document.getElementById( 'files-filename' );
 
+input.addEventListener( 'change', showFileName );
+
+function showFileName( event ) {
+
+  // the change event gives us the input it occurred in
+  var input = event.srcElement;
+
+  // the input has an array of files in the `files` property, each one has a name that you can use. We're just using the name here.
+  var fileName = input.files[0].name;
+
+  // use fileName however fits your app best, i.e. add it into a div
+  infoArea.textContent = 'File name: ' + fileName;
+}
 (function($) {
 
 	var	$window = $(window),
