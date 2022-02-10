cdd6e4942b24a47b3a0d89238b85ec40800a0a35
Arnav Jain <arnavj22@gmail.com>
2022-02-09 21:20:56 -0500

    added favicon

--- website/run.py
+++ website/run.py
@@ -15,6 +15,10 @@ matplotlib.use('Agg')
 app = Flask(__name__)
 
 
+@app.route('/favicon.ico')
+def favicon():
+    return app.send_static_file('favicon.ico')
+
 @app.route('/')
 def home():
     return render_template('index.html')
